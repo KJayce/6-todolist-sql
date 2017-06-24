e4cf932518a4c087c7e3af56b68e4eb142931523
KJayce <jaycekaje@gmail.com>
2017-06-24 12:22:30 +0200

    text change performed

--- Training/app.js
+++ Training/app.js
@@ -1,6 +1,15 @@
 
 
-var changeText=function(){
-    var hits = 0;
-;
-}
+    var counter = 0;
+    var quotes = ["Hello there", "Hi again", "This is getting boring", "Stop it now!!!", "Why do you do that?","I am just going to ignore you now", "..."]
+    
+    var end = quotes.length -1;
+    function changeText(){
+        if (counter == end){
+            counter= 0;
+        }else{
+            counter++;
+        }document.getElementById('cham').innerHTML=quotes[counter];
+            console.dir(counter);
+    }
+        
